0887b8e7010d5853f885afa0b753a6751c565a25
Stephen Suen <ssuen@mit.edu>
2013-05-09 14:05:59 -0400

    Added arduino code

---
+++ monkeybiz.ino
@@ -0,0 +1,22 @@
+int TIME_LIMIT;
+
+class Skyscraper {
+  private:
+    
+  public:
+    Servo getServo();
+    
+  
+}
+
+// Sets up the game logic.
+void setup() {
+
+}
+
+// Main loop.
+void loop() {
+
+}
+
+
